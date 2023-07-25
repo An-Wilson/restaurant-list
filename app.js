f5fd0e6445c307396fc402f51c2d32c490780e58
An-Wilson <ta102779@gmail.com>
2023-07-25 16:59:45 +0800

    refactor: index  & feat: add show page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurants.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -10,9 +11,15 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurantList.results.find(
+    restaurant => restaurant.id.toString() === req.params.id
+  )
+  res.render('show', { restaurant: restaurant })
+})
 
 app.listen(port, () => {
   console.log(`App is listening on localhost:${port}`)
