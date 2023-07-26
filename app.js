2d635fa25db4b33efc5fb89d883ff8de21f2c0a4
An-Wilson <ta102779@gmail.com>
2023-07-26 18:40:59 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+// includes Express modules related variables
 const express = require('express')
 const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+
+// includes Mongoose related variables
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 const restaurantList = require('./restaurants.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
