d1c3c946d86b17d6ef8e83787f5e6e7e2b00c9c1
An-Wilson <ta102779@gmail.com>
2023-08-11 15:47:13 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 // 引入路由模組
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 // 依網址結構導向個別模組
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
