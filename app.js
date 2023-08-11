f8460fb219023dc16f390db76287da3324428636
An-Wilson <ta102779@gmail.com>
2023-08-11 18:05:17 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // includes Express modules related variables
 const express = require('express')
 const session = require('express-session')
-const app = express()
-const port = 3000
-
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')  // 載入 Restaurant Model
 const methodOverride = require('method-override')
+
 const routes = require('./routes')  // 預設自動找 index.js
+
+const usePassport = require('./config/passport')
 require('./config/mongoose')
+
+const app = express()
+const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -22,8 +25,10 @@ app.use(session({
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(routes)
 
+usePassport(app)
+
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`App is listening on localhost:${port}`)
---
+++ config/passport.js
@@ -0,0 +1,37 @@
+// 1. 先載入相關模組
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+// 2. module.exports 並初始化套件
+module.exports = app => {
+  // 初始化 passport 模組
+  app.use(passport.initialize())
+  app.use(passport.session())
+  // 設定本地登入策略
+  passport.use(new LocalStrategy(
+    { usernameField: 'email' }, (email, password, done) => {
+      User.findOne({ email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: '此帳號不存在' })
+          }
+          if (user.password !== password) {
+            return done(null, false, { message: '密碼核對錯誤' })
+          }
+          return done(null, user)
+        })
+        .catch(err => done(err, false))
+    }))
+
+  // 設定序列化及反序列化
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then(user => done(null, user))
+      .catch(user => done(err, null))
+  })
+}
