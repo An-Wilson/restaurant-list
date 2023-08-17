0d50421c6ca19ded1e65fcb8d23461b39dce88ea
An-Wilson <ta102779@gmail.com>
2023-08-17 18:07:45 +0800

    feat: add sort function in index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,11 +4,18 @@ const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
 
+const sorts = [
+  { sortName: "A - Z", sortBy: "a-z" },
+  { sortName: "Z - A", sortBy: "z-a" },
+  { sortName: "類別", sortBy: "category" },
+  { sortName: "地區", sortBy: "location" }
+]
+
 router.get('/', (req, res) => {
   Restaurant.find({ userId: req.user._id }) // 取出 Restaurant Model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .sort({ _id: "asc" })
-    .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
+    .then(restaurants => res.render('index', { restaurants, sorts })) // 將資料傳給 index 樣板
     .catch(err => console.error(err))
 })
 
@@ -23,6 +30,28 @@ router.get('/search', (req, res) => {
       })
       res.render('index', { restaurants: filteredRestaurants, keyword })
     })
+    .catch(err => console.error(err))
+})
+
+// 功能：首頁排序功能
+router.get('/sort/:sortBy', (req, res) => {
+  const sortBy = req.params.sortBy
+  let sort = {}
+
+  if (sortBy === "a-z") {
+    sort = { name: "asc" }
+  } else if (sortBy === "z-a") {
+    sort = { name: "desc" }
+  } else if (sortBy === "category") {
+    sort = { category: "asc" }
+  } else if (sortBy === "location") {
+    sort = { location: "asc" }
+  }
+
+  Restaurant.find({ userId: req.user._id })
+    .lean()
+    .sort(sort)
+    .then(restaurants => res.render('index', { restaurants, sorts }))
     .catch(err => console.error(err))
 })
 
